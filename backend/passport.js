a7b075457f95a98390b14f0388dce51642046db2
ZohaibAliJerov <80768304+ZohaibAliJerov@users.noreply.github.com>
2024-02-06 18:02:53 +0500

    Update passport.js
    change some web token

--- backend/passport.js
+++ backend/passport.js
@@ -3,22 +3,12 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 // const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
-// const GOOGLE_CLIENT_ID =
-//   "13891966248-p5o9n8d33slcv8dhnsv7ljp1ba4k7s6t.apps.googleusercontent.com";
-// const GOOGLE_CLIENT_SECRET = "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";
 
-// TWITTER_App_ID = "your id";
-// TWITTER_APP_SECRET = "your id";
-
-// FACEBOOK_APP_ID = "726030618886450";
-// FACEBOOK_APP_SECRET = "cf3a9d450dd84fbdd44b8bd683cc82ab";
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID:  "13891966248-p5o9n8d33slcv8dhnsv7ljp1ba4k7s6t.apps.googleusercontent.com",
-      clientSecret:  "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",
-      callbackURL: "/auth/google/callback",
+      
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
